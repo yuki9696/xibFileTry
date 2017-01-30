c0b79947897e1fad44bb538892ba71d167ff6844
yuki9696 <yukiyamamoto@yuki-no-macbook-pro.local>
2017-01-30 22:05:45 +0900

    Initial Commit

---
+++ CustomCells1:30/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomCells1:30
+//
+//  Created by yuki yamamoto on H29/01/30.
+//  Copyright © 平成29年 yuki yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CustomCells1:30Tests/CustomCells1_30Tests.swift
@@ -0,0 +1,36 @@
+//
+//  CustomCells1_30Tests.swift
+//  CustomCells1:30Tests
+//
+//  Created by yuki yamamoto on H29/01/30.
+//  Copyright © 平成29年 yuki yamamoto. All rights reserved.
+//
+
+import XCTest
+@testable import CustomCells1_30
+
+class CustomCells1_30Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
